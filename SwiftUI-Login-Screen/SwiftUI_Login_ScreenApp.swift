fc7bdabd67838dc3c3c82afc2e8ff1faa32c7416
Ethan Halprin <ethan.halprin@gmail.com>
2022-02-25 13:59:39 +0200

    Initial Commit

---
+++ SwiftUI-Login-Screen/SwiftUI_Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Login_ScreenApp.swift
+//  SwiftUI-Login-Screen
+//
+//  Created by Ethan on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
